c9ba1c8bb71c07926944ea4ba235061b7069092f
erincr <erinc@Erins-MacBook-Pro.local>
2015-11-13 14:11:11 -0500

    First draft of new shiny app.

--- shiny/map_app/ui.R
+++ shiny/map_app/ui.R
@@ -1,47 +1,89 @@
-#ref: https://arilamstein.shinyapps.io/reference-map-toy
-
-
 library(shiny)
-library(choroplethr)
-library(choroplethrZip)
 
 
-data(continental_us_states)
-states = continental_us_states
-vars = c("num.discharges", "covered.charges", "total.payments", "medicare.payments")
-         #"medicare.frac"
+drg.codes = c("057", "064", "065", "066", "069", "101", "176", "177", "178", "189", "190", "191", "192", "193", "194",
+              "195", "202", "203", "208", "243", "280", "281", "282", "291", "292", "293", "300", "305", "308", "309",
+              "310", "312", "313", "329", "330", "372", "377", "378", "379", "389", "390", "391", "392", "394", "419",
+              "470", "481", "482", "536", "552", "563", "603", "637", "638", "640", "641", "682", "683", "684", "689",
+              "690", "811", "812", "853", "871", "872", "897", "918", "948", "149", "207", "303", "314", "315", "331",
+              "439", "460", "698", "699", "917", "074", "244", "253", "287", "469", "473", "480", "491", "602", "039",
+              "100", "238", "246", "247", "249", "251", "252", "254", "286", "301", "371", "418", "467", "484", "870",
+              "885")
 
-shinyUI(fluidPage(
+states = list( 
+  'Alaska'=                  'AK', 
+  'Alabama'=                 'AL', 
+  'Arkansas'=                'AR', 
+  'American Samoa'=          'AS', 
+  'Arizona'=                 'AZ', 
+  'California'=              'CA', 
+  'Colorado'=                'CO', 
+  'Connecticut'=             'CT', 
+  'District of Columbia'=    'DC', 
+  'Delaware'=                'DE', 
+  'Florida'=                 'FL', 
+  'Georgia'=                 'GA', 
+  'Guam'=                    'GU', 
+  'Hawaii'=                  'HI', 
+  'Iowa'=                    'IA', 
+  'Idaho'=                   'ID', 
+  'Illinois'=                'IL', 
+  'Indiana'=                 'IN', 
+  'Kansas'=                  'KS', 
+  'Kentucky'=                'KY', 
+  'Louisiana'=               'LA', 
+  'Massachusetts'=           'MA', 
+  'Maryland'=                'MD', 
+  'Maine'=                   'ME', 
+  'Michigan'=                'MI', 
+  'Minnesota'=               'MN', 
+  'Missouri'=                'MO', 
+  'Northern Mariana Islands'='MP', 
+  'Mississippi'=             'MS', 
+  'Montana'=                 'MT', 
+  'National'=                'NA', 
+  'North Carolina'=          'NC', 
+  'North Dakota'=            'ND', 
+  'Nebraska'=                'NE', 
+  'New Hampshire'=           'NH', 
+  'New Jersey'=              'NJ', 
+  'New Mexico'=              'NM', 
+  'Nevada'=                  'NV', 
+  'New York'=                'NY', 
+  'Ohio'=                    'OH', 
+  'Oklahoma'=                'OK', 
+  'Oregon'=                  'OR', 
+  'Pennsylvania'=            'PA', 
+  'Puerto Rico'=             'PR', 
+  'Rhode Island'=            'RI', 
+  'South Carolina'=          'SC', 
+  'South Dakota'=            'SD', 
+  'Tennessee'=               'TN', 
+  'Texas'=                   'TX', 
+  'Utah'=                    'UT', 
+  'Virginia'=                'VA', 
+  'Virgin Islands'=          'VI', 
+  'Vermont'=                 'VT', 
+  'Washington'=              'WA', 
+  'Wisconsin'=               'WI', 
+  'West Virginia'=           'WV', 
+  'Wyoming'=                  'WY'
+)
 
-  titlePanel("Hospital Charge Data Exploration"),
-  div(HTML("Select state, map color, and variable. The variables 'covered.charges', 'total.payments', 'medicare.payments' are normalized by 
-           each by Zip code's population of age 65 and older, where available. ")),
-  
-  sidebarLayout(
+shinyUI(
+  verticalLayout(
+    
+    # Application title
+    headerPanel("hi friends"),
+    
     sidebarPanel(
-      
-      radioButtons(inputId = "color",
-                  label    = "Color:",
-                  choices  = c("color", "bw"),
-                  selected = "bw"),
-      
-      checkboxInput(input = "reference_map",
-                    label = "Include Reference Map",
-                    value = TRUE),
-      
-      selectInput(inputId  = "state",
-                  label    = "States",
-                  choices  = states,
-                  selected = "Ohio"),
-      
-      selectInput(inputId  = "var",
-                  label    = "Variable",
-                  choices  = vars,
-                  selected = "total.payments")
+      selectInput("statename", "State:", states),
+      selectInput("drgcode", "code:", drg.codes)
     ),
     
     mainPanel(
-      imageOutput("map", width="640px", height="480px")
+        plotOutput("ggPlot")
     )
+    
   )
-))
+)
